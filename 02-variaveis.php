db5dd7ce26cef2b8e9446e029f7b76149a9f6c90
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2022-10-10 19:58:01 -0300

    Initial commit

---
+++ 02-variaveis.php
@@ -0,0 +1,31 @@
+<?php
+//Meus Dados
+
+$nome = "Maria Fernanda";
+$idade = 23;
+$altura = 1.54;
+
+echo "meu nome é $nome, minha idade é $idade, minha altura é $altura <br>";
+
+$nome =  "Nayyr quebra códigos";
+$idade = 28;
+$altura = 1.75;
+
+
+echo "meu nome é $nome, minha idade é $idade, minha altura é $altura <br>";
+echo "<hr>";
+
+$carro = "Hillux";
+echo $carro;
+
+$Carro = "Fusca <br>";
+echo $Carro;
+
+$meuNome = " genoveva ";
+echo $meuNome;
+
+//Nomes válidos
+// $nome
+// $_nome
+// $carro10
+// $meuNome
