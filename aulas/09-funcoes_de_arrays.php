ddbeefc09db37b1aa7e88793c418d2087a545453
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2022-10-12 21:36:31 -0300

    aaula 09

---
+++ aulas/09-funcoes_de_arrays.php
@@ -0,0 +1,20 @@
+<?php
+// is_array($array) = verificar se uma determinada variável é um array
+
+
+
+
+
+
+// in_array($valor, array) = verificar se um determinado valor existe em alguma posição do array
+// array_keys($array) = retorna um novo array com as chaves do array passando como parâmetro
+// array_values($array) = retorna um novo array com os valores do array passando como parâmetro
+// array_merge($array1, $array2) = agrega o conteúdo dos dois arrays
+// array_pop($array) = exclui a última posição do array
+// array_shift($array) = exclui a primeira posição do array
+// array_unshift($arr, "valor") = adiciona mais um elemento no início do array 
+// array_push($array, "valor", "valor") = adiciona um ou mais elementos no final do array
+// array_combine($keys, $values) = mescla dois arrays passados
+// array_sum() = calcula a soma dos elementos de um array
+// explode("/", "20/01/2001") = transforma string em array
+// implode("-", arr) = transforma array em string
